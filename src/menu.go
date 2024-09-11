7da179845902235169e266ce7f246b6c439c38e4
antony <antony.fontaine@ynov.com>
2024-09-11 16:27:22 +0200

    menu fini

--- src/menu.go
+++ src/menu.go
@@ -1,14 +1,14 @@
 package main
 
 import( 
-"ftm"
+"fmt"
 "os"
 "bufio"
 "strings"
 
 )
 
-type personnage struct {
+type Personnage struct {
 	Nom 	string
 	Classe	string 
 	Niveau	int 
@@ -23,7 +23,7 @@ func NouveauPersonnage(nom, classe string) *Personnage {
 		Nom: 	nom,
 		Classe: 	classe,
 		Niveau:		1,
-		ptsVie:	100,
+		PtsVie:	100,
 	PtsAttaque: 10,
 	PtsDefense: 5,
 	Inventaire: []string{"Potion de soin", "épée courte", "Bouclier en bois"},
@@ -31,6 +31,46 @@ func NouveauPersonnage(nom, classe string) *Personnage {
 }
 
 func (p *Personnage) Afficherinfo() {
-	ftm.Printf("Nom: %s\n", p.Nom)
-	ftm.Printf("Classe: %s\n")
+	fmt.Printf("Nom: %s\n", p.Nom)
+	fmt.Printf("Classe: %s\n, " p.Classe)
+	fmt.Printf("Niveau: %d\n" p.Niveau)
+	fmt.Printf("Points de vie: %d\n", p.PtsVie)
+	fmt.Printf("Points d'attaque: %d\n", p.PtsAttaque)
+	fmt.Printf("Points de défense: %d\n",p.PtsDefense)
+}
+func (p *Personnage) AfficherInventaire()
+fmt.Println("Inventaire:")
+for _, item := range p.Inventaire {
+	fmt.Println("-", item)
+	}
+}
+
+func main() {
+	joueur := NouveauPersonnage("Héros","Guerrier")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		afficherMenu()
+		fmt.Print("Choisissez une option:")
+		scanner.Scan()
+		choix := strings.TrimSpace(scanner.Text())
+		switch choix {
+		case "1":
+			joueur.AfficherInfo()
+		case"2":
+			joueur.AfficherInventaire()
+		case"3":
+			fmt.Println(Merci d'avoir joué. Au revoir!")
+			return
+		default:
+			fmt.Println("Option invalide. Veuiller choisir 1, 2 ou 3.")
+		
+		}
+	}
+}
+func afficherMenu(){
+fmt.Println("\nMenu Principal")
+fmt.Println("1. Afficher les informations du personnage")
+fmt.Println("2. Accéder au contenu de l'inventaire")
+fmt.Println("3. Quitter")
 }
